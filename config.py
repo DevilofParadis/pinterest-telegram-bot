d2f2f0ddb896195ed413c9f083901009e116cc6e
Pandey Amit <114922080+DevilofParadis@users.noreply.github.com>
2023-10-21 19:57:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ HEADERS = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
 }
-TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = os.getenv("6116413872:AAHwJ-3a2oefzMhTHsVE6ppbrirUY2K5488")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
-REDIS_URL = os.getenv("REDIS_URL")
+REDIS_URL = os.getenv("redis://default:trial@01@redis-13297.c232.us-east-1-2.ec2.cloud.redislabs.com:13297")
 WORKER_URL = os.getenv("WORKER_URL")
-WORKER_HEADERS = {"X-API-Key": os.getenv("WORKER_API_KEY")}
+WORKER_HEADERS = {"25374144": os.getenv("WORKER_API_KEY")}
 BLOCKED_USERS = list(map(int, os.getenv("BLOCKED_USERS", "0").split(",")))
 YT_VIDEOS = [
     "https://youtu.be/w0F6DYxbegs",
